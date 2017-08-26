3b1a626cf010d1245acbaf09e79e4b943b55d2c0
thomhines <thomhines@gmail.com>
2017-08-26 09:44:08 -0700

    Clear animation queue when jumping to another image so that animations don't stack up when browser tab is idle

--- square1.js
+++ square1.js
@@ -1,12 +1,12 @@
-/**
- * Square1 Slider
- * Copyright (c) 2016 Thom Hines
- * Licensed under MIT.
- * @author Thom Hines
- * https://github.com/thomhines/square1
- * @version 0.1
- */
-  
+/*
+* Square1 Slider
+* Copyright (c) 2016 Thom Hines
+* Licensed under MIT.
+* @author Thom Hines
+* https://github.com/thomhines/square1
+* @version 0.1.1
+*/
+
 // todo:
 // Work with velocity.js and fall back to jQuery if not available
 // Add 'slide' transition
@@ -15,19 +15,19 @@
 
 
 (function($){
-	
+
 $.fn.square1 = function(options) {
-	
+
 	var _this=this;
 	var settings;
 	var square1_interval;
 	var slideshow_hover = false;
-	
+
 
 	// Stop plugin execution if selected elements aren't present on page
 	if(_this.length < 1) return;
-	
-	
+
+
 	// Load settings from element data, or set up default settings and store data in element
 	if($(_this).data('settings')) {
 		var settings = $(_this).data('settings');
@@ -51,7 +51,7 @@ $.fn.square1 = function(options) {
 			onStop: 			function() {},
 			onChange: 			function() {}
 		},options);
-		
+
 		$(_this).data('settings', settings);
 	}
 
@@ -59,7 +59,7 @@ $.fn.square1 = function(options) {
 
 
 
-	
+
 	// HANDLE SLIDESHOW CONTROL METHODS
 	if(options == 'play') {
 		run_slideshow();
@@ -71,39 +71,39 @@ $.fn.square1 = function(options) {
 		stop_slideshow();
 		return;
 	}
-	
+
 	if(options == 'next') {
 		next_image();
 		if(settings['auto_start']) reset_interval();
 		return;
 	}
-	
+
 	if(options == 'prev') {
 		prev_image();
 		if(settings['auto_start']) reset_interval();
 		return;
 	}
-	
+
 	// if value is an integer, go to that image
 	if(Math.floor(options) === options && +options === options) {
 		jump_to_image(options-1);
 		if(settings['auto_start']) reset_interval();
 		return;
 	}
-	
-	
 
 
 
 
-		
+
+
+
 	// STYLE SLIDESHOW ELEMENTS
 	$(_this).css({
 		position: 'relative',
 		width: settings['width'],
 		height: settings['height'],
 	});
-	
+
 
 	// CREATE SLIDESHOW ELEMENTS
 
@@ -111,28 +111,28 @@ $.fn.square1 = function(options) {
 
 	// Surround all direct decendents with <div> (so that this can work with images or other elements, such as <a> or <ul>)
 	$(_this).children().wrap('<div class="image_wrapper" />');
-	
+
 	$('.image_wrapper', _this).each(function() {
 		var img = $(this).find('img').attr('src');
 		$(this).css('background-image', 'url('+img+')');
 	});
-	
+
 	// Hide all but first image, then fade them in one at a time.
 	$('.image_wrapper:first', _this).addClass('current_slide');
 	$('.image_wrapper:not(.current_slide)', _this).hide();
-	
+
 	$(_this).append('<div class="square1_caption"></div>');
-	
+
 	// Add slideshow navigation controls
 	$(_this).append('<div class="square1_controls"><span class="square1_prev_image">Previous Image</span><span class="square1_next_image">Next Image</span><div class="square1_dots"></div></div>');
-	
+
 	// For each img, add dot to dot nav
 	var x = 0;
 	$('.image_wrapper', _this).each(function() {
 		$('.square1_dots', _this).append('<span data-image-num="' + x + '"></span>');
 		x++;
-	}); 
-	
+	});
+
 	// Customize behavior styles
 	$(_this).addClass('fill_mode-' + settings['fill_mode']);
 	$(_this).addClass('theme-' + settings['theme']);
@@ -143,68 +143,68 @@ $.fn.square1 = function(options) {
 
 
 
-	
+
 	// EVENTS
-	
+
 	$('.square1_prev_image', _this).click(function() {
 		prev_image();
 		if(settings['auto_start']) reset_interval();
 	});
-	
+
 	$('.square1_next_image', _this).click(function() {
 		next_image();
 		if(settings['auto_start']) reset_interval();
 	});
-	
+
 	$('.square1_dots span', _this).click(function() {
 		jump_to_image($(this).data('image-num'));
-		if(settings['auto_start']) reset_interval();	
+		if(settings['auto_start']) reset_interval();
 	})
-	
+
 	// pause on hover
-	$(_this).on('mouseenter', '.image_wrapper', function() { 
-		if(settings['pause_on_hover']) stop_slideshow(); 
-	});
-	
-	$(_this).on('mouseleave', '.image_wrapper', function() { 
-		if(settings['pause_on_hover'] && settings['auto_start']) run_slideshow(); 
+	$(_this).on('mouseenter', '.image_wrapper', function() {
+		if(settings['pause_on_hover']) stop_slideshow();
 	});
 
-	
+	$(_this).on('mouseleave', '.image_wrapper', function() {
+		if(settings['pause_on_hover'] && settings['auto_start']) run_slideshow();
+	});
+
+
 	// start slideshow (with pause at the beginning)
 	if(settings['auto_start']) setTimeout(run_slideshow, settings['start_delay']);
 	update_caption();
 
 
-	
-	
+
+
 	// HELPER FUNCTIONS
-	
+
 	// Start slideshow
 	function run_slideshow() {
 		reset_interval();
 		update_dots_nav();
 		settings['onPlay']();
 	}
-	
+
 	// Stop slideshow
 	function stop_slideshow() {
 		clearInterval(square1_interval);
 		settings['onStop']();
 	}
-	
+
 	// Reset timer for slideshow (to prevent weird jumps when changing slides, for instance)
 	function reset_interval() {
-		clearInterval(square1_interval); 
+		clearInterval(square1_interval);
 		square1_interval = setInterval(next_image, settings['slide_duration'] + settings['transition_time']);
 		$(_this).data('interval', square1_interval);
 	}
-	
-	
+
+
 	// Move to next slide
 	function next_image() {
 		var curr_slide_index = $('.current_slide', _this).index();
-		
+
 		// If last slide, show first slide instead
 		if(curr_slide_index == $('.image_wrapper', _this).length - 1) jump_to_image(0);
 
@@ -212,21 +212,24 @@ $.fn.square1 = function(options) {
 		else jump_to_image(curr_slide_index + 1);
 	}
 
-	
+
 	// Move to previous slide
 	function prev_image() {
 		var curr_slide_index = $('.current_slide', _this).index();
 
 		// If first slide, show final slide
 		if(curr_slide_index == 0) jump_to_image($('.image_wrapper', _this).last().index());
-		
+
 		// Otherwise, show previous slide
 		else jump_to_image(curr_slide_index - 1);
 	}
-	
+
 	// Jump to specific image
 	function jump_to_image(image_num) {
-		
+
+		// Cancel all previous animations
+		$('.image_wrapper', _this).clearQueue();
+
 		// Different fading necessary for different fill modes. Crossfading in 'cover' mode requires that both elements are visible at the same time
 		if(settings['fill_mode'] == 'cover') {
 			// If index of new image is bigger than current, fade in new image and hide current
@@ -234,25 +237,25 @@ $.fn.square1 = function(options) {
 				$('.current_slide', _this).removeClass('current_slide');
 				$('.image_wrapper', _this).eq(image_num).addClass('current_slide').fadeIn(settings['transition_time'], function() { $('.image_wrapper:not(.current_slide)', _this).hide(); });
 			}
-			
+
 			// Otherwise, show new image and fade out current
-			else if(image_num < $('.current_slide', _this).index()) {			
+			else if(image_num < $('.current_slide', _this).index()) {
 				$('.current_slide', _this).removeClass('current_slide').fadeOut(settings['transition_time']);
 				$('.image_wrapper', _this).eq(image_num).addClass('current_slide').show();
-			}	
+			}
 		}
-		
+
 		// Whereas 'contain' mode looks weird when different sized images stay on the screen too long
 		else {
 			$('.current_slide', _this).removeClass('current_slide').fadeOut(settings['transition_time']);
 			$('.image_wrapper', _this).eq(image_num).addClass('current_slide').fadeIn(settings['transition_time']);
 		}
-		
+
 		update_caption();
 		update_dots_nav();
 		settings['onChange']();
 	}
-	
+
 	// Update image caption
 	function update_caption() {
 		$('.square1_caption', _this).fadeOut(settings['transition_time'], function() {
@@ -261,7 +264,7 @@ $.fn.square1 = function(options) {
 			}, 10)
 		});
 	}
-	
+
 	// Update dots navigation position
 	function update_dots_nav() {
 		$('.square1_dots span', _this).removeClass('current');
